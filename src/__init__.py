40ac35b215380458c74ce64c5bfbdbbefdc34c23
Anuj Datar <anuj.datar@gmail.com>
2023-09-29 00:47:21 +0530

    Fix: Changed import order in src/__init__, due to circular import error

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 from .types import Key, KeymapTab
 from .load_keymap import keymap_json_loader
-from .AppUi import PiDeckUi
 from .serial_comm import send_serial_msg
+
+from .AppUi import PiDeckUi
 
 __all__ = [
     "Key",
