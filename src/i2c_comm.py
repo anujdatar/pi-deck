413f72556e4508ced36b68af25213777f140f1fc
Anuj Datar <anuj.datar@gmail.com>
2023-10-13 20:11:34 +0530

    an alternative way of sending data over i2c, instead of utf-8 encode
    i think this goes as a list of integers, works the same though

--- src/i2c_comm.py
+++ src/i2c_comm.py
@@ -10,6 +10,9 @@ def send_i2c_msg(message: str):
     I2Cbus.write_i2c_block_data(arduino_address, 0, list(data_to_send))
     time.sleep(0.1)
 
+    # data_to_send = [ord(char) for char in message]
+    # I2Cbus.write_i2c_block_data(arduino_address, 0, data_to_send)
+
 
 if __name__ == "__main__":
     send_i2c_msg("test")
