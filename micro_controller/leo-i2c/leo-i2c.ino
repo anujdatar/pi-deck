d103f6b30658bc608ca196c0c28bdbb0234174a1
Anuj Datar <anuj.datar@gmail.com>
2023-10-20 21:25:41 +0530

    changed the media control ifs to reflect new shorter commands
    and commented out serial code

--- micro_controller/leo-i2c/leo-i2c.ino
+++ micro_controller/leo-i2c/leo-i2c.ino
@@ -9,8 +9,7 @@ void setup() {
   Wire.begin(11);
   Wire.onReceive(receiveData);
 
-  // Serial1.begin(115200);
-  Serial.begin(115200);
+  // Serial.begin(115200);
 
   Media_Keyboard.begin();
 }
@@ -28,21 +27,21 @@ void receiveData(int bytecount) {
   // receivedData.replace("\0", "");
   receivedData.remove(0,1);
 
-  Serial.println(receivedData);
+  // Serial.println(receivedData);
 
   if (receivedData == "v+") {
     Media_Keyboard.write(KEY_VOLUME_UP);
   } else if (receivedData == "v-") {
     Media_Keyboard.write(KEY_VOLUME_DOWN);
-  } else if (receivedData == "mute") {
+  } else if (receivedData == "mt") {
     Media_Keyboard.write(KEY_MUTE);
-  } else if (receivedData == "pause") {
+  } else if (receivedData == "pp") {
     Media_Keyboard.write(KEY_PLAY_PAUSE);
-  } else if (receivedData == "stop") {
+  } else if (receivedData == "st") {
     Media_Keyboard.write(KEY_STOP);
-  } else if (receivedData == "next") {
+  } else if (receivedData == "t+") {
     Media_Keyboard.write(KEY_NEXT_TRACK);
-  } else if (receivedData == "prev") {
+  } else if (receivedData == "t-") {
     Media_Keyboard.write(KEY_PREVIOUS_TRACK);
   } else {
     for (int i = 0; i < receivedData.length(); i++) {
